e20c0549cc21a81e9ae0a9bcc5d87edada0d1a6f
Flavio Franco Tunqui <flavio.franco.tunqui@fisglobal.com>
2018-12-03 17:54:41 -0500

    list basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'demo/dynamicList.dart';
 import 'cookbook/animation/fadeInOut.dart';
+import 'cookbook/lists/basic.dart';
+import 'cookbook/lists/horizontal.dart';
 
-void main() => runApp(FadeInOut());
+void main() => runApp(HorizontalList());
